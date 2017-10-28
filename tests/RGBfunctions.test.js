7af456007b6aa65d2d88e87acd276e3a404c142e
Your Name <ian.sudderth@gmail.com>
2017-10-28 00:51:33 -0400

    adds boiler and test boiler for RGB function

---
+++ tests/RGBfunctions.test.js
@@ -0,0 +1,36 @@
+var { rgb, rgba, red, green, blue, mix } = require('../lib/RGBfunctions');
+
+var {
+  hsvFormat,
+  hsvaFormat,
+  hslFormat,
+  hslaFormat,
+  hexFormat,
+  hex8Format,
+  rgbFormat,
+  rgbaFormat,
+} = require('./formatRegEx');
+
+describe('rgb', () => {
+  it('returns a string', () => {});
+});
+
+describe('rgba', () => {
+  it('returns a string', () => {});
+});
+
+describe('red', () => {
+  it('returns a string', () => {});
+});
+
+describe('green', () => {
+  it('returns a string', () => {});
+});
+
+describe('blue', () => {
+  it('returns a string', () => {});
+});
+
+describe('mix', () => {
+  it('returns a string', () => {});
+});
