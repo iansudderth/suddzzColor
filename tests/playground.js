adcf752b4fd856e7e9c323bceb2649a2caea3f77
Your Name <ian.sudderth@gmail.com>
2017-11-11 17:27:33 -0500

    implements fadeOutGradient, darkenGradient, lightenGradient, and tests. passes all tests

--- tests/playground.js
+++ tests/playground.js
@@ -1,14 +1,4 @@
-const { invert } = require('../index');
+const { mapAmountOverArray } = require('../lib/internalUtilites');
+const { fadeOut } = require('../index');
 
-color = '#FF0000';
-
-console.log(invert(color, 0));
-console.log(invert(color, 50));
-console.log(invert(color, 100));
-console.log(invert(color));
-
-color = '#F0A1B9';
-console.log(invert(color, 50));
-
-color = '#0AA00A';
-console.log(invert(color, 50));
+console.log(fadeOut('rgb(255,0,0)', 0.5));
